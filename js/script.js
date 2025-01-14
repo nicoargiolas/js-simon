dc2a2852471874814686e60c52251633617055df
nicoargiolas <argiolas.nicola00@gmail.com>
2025-01-14 16:01:34 +0100

    Generazione numeri randomici e switch parte visibile

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@
 
 // Prendo gli elementi che mi servono dall'html
 const countdown = document.getElementById('countdown');
+const instructions = document.getElementById('instructions');
 const randomNumbers = document.getElementById('numbers-list');
 const form = document.getElementById('answers-form');
 const userNumbers = document.querySelectorAll('.form-control');
@@ -16,22 +17,47 @@ const button = document.querySelector('.btn');
 const message = document.getElementById('message');
 
 // Imposto il countdown
-let time = 30
+let time = 3
 
 const clock = setInterval(() => {
     // Se il tempo arriva a 0 fermo il countdown e nascondo il div di partenza
     if (time === 0) {
         clearInterval(clock);
+        countdown.classList.add("d-none");
+        randomNumbers.classList.add("d-none");
+        form.classList.remove("d-none");
+        instructions.innerText = 'Scrivi i numeri'
     } else {
     countdown.innerText = time
     time = time - 1;
     }
 }, 1000);
 
-// Lo fermo quando arriva a 0
-
 
 // Genero i numeri randomici
+
+// Funzione numeri randomici
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Ciclo per aggiungere i numeri randomici con controllo che non ce ne siano uguali
+const arrayRandomN = [];
+for (let i = 0; arrayRandomN.length < 5; i++) {
+    let n = getRandomArbitrary(1,50);
+
+    if (arrayRandomN.includes(n)) {} else {
+        arrayRandomN.push(n);
+    }
+}
+
+// Stampo in pagina gli elementi dell'array
+for (let i = 0; i < arrayRandomN.length; i++) {
+    randomNumbers.innerHTML += `<li> ${arrayRandomN[i]} </li>`;
+}
+
+
+
 
 // Alla fine del countdown nascondo countdown e numeri e faccio comparire le caselle di input
 
